1981df00adcf7719a7f854b1ad8f3e16d4c3d447
tan90xx <592026773@qq.com>
2023-03-07 11:15:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+# ------------------ prepdata --------------------------
+sample_rate = 16000
+stride_samples = sample_rate * 15
+clip_samples = sample_rate * 30
+kfold = 4
+
+labels = ['Cargo', 'Passengership', 'Tanker', 'Tug']
+lb_to_idx = {lb: idx for idx, lb in enumerate(labels)}
+idx_to_lb = {idx: lb for idx, lb in enumerate(labels)}
+classes_num = len(labels)
 # ------------------dft parameters---------------------
 FFT = 16000
 HOP = 160
