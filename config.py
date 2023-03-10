c3c411bb8c82871585c7074aa5c6c6eb3dcd1106
tan90xx <592026773@qq.com>
2023-03-10 13:47:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ labels = ['Cargo', 'Passengership', 'Tanker', 'Tug']
 lb_to_idx = {lb: idx for idx, lb in enumerate(labels)}
 idx_to_lb = {idx: lb for idx, lb in enumerate(labels)}
 classes_num = len(labels)
+
+full_samples_per_class = np.array([2410,  2861,   2703,  2599])
+
 # ------------------dft parameters---------------------
 FFT = 16000
 HOP = 160
